1dbe4b9787d5c8b6dc0314fe2cbb6f833f92371e
Dan Rowe <draco2003@gmail.com>
2019-02-07 19:17:30 -0500

    Update based on source example pull request

--- webui.py
+++ webui.py
@@ -68,4 +68,4 @@ def handle_logging(client, userdata, level, buf):
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, use_reloader=True, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, use_reloader=False, debug=True)
